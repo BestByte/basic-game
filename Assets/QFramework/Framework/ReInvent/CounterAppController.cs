e57ae2c1c4d4df7795cb00f11297c99d4421ada4
ilxna <ilvxna@hotmail.com>
2023-02-01 16:44:31 +0800

    :art: add view

--- Assets/QFramework/Framework/ReInvent/CounterAppController.cs
+++ Assets/QFramework/Framework/ReInvent/CounterAppController.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace QFramework.Reinvent { 
 public class CounterAppController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+        #region view
+
+        private Button mBtnAdd;
+        private Button mBtnSub;
+        private Text mCounterTxt;
+
+		#endregion
+
+		// Start is called before the first frame update
+		void Start()
     {
         
     }
