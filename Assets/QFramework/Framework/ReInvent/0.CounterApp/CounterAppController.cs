82aec279f9cd51dbdb5a40ae0b2707f1b81a2764
ilxna <ilvxna@hotmail.com>
2023-02-06 21:22:37 +0800

    2.定义一个架构（提供 MVC、分层、模块管理等）

--- Assets/QFramework/Framework/ReInvent/0.CounterApp/CounterAppController.cs
+++ Assets/QFramework/Framework/ReInvent/0.CounterApp/CounterAppController.cs
@@ -53,10 +53,35 @@ namespace QFramework.MVC
 {
     using QFramework;
 
-    public class CounterAppModel:AbstractModel
+	#region 1. 定义一个 Model 对象
+	public class CounterAppModel:AbstractModel
     {
         public int Count;
+		protected override void OnInit()
+		{
+			Count= 0;
+		}
 
-    }
+	}
+	#endregion
 
+	#region  2.定义一个架构（提供 MVC、分层、模块管理等）
+	public class CounterApp : Architecture<CounterApp>
+	{
+		protected override void Init()
+		{
+			this.RegisterModel(new CounterAppModel());
+		}
+	}
+	#endregion
+
+	#region Controller
+	public class CounterAppController : MonoBehaviour, IController
+	{
+		public IArchitecture GetArchitecture()
+		{
+			throw new NotImplementedException();
+		}
+	}
+	#endregion
 }
