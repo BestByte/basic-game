84c3ecfe96740fd8fc1c69a94e1fa5a05f920e8a
ilxna <ilvxna@hotmail.com>
2023-02-25 15:47:29 +0800

    add codeGENkit :art:

---
+++ Assets/QFramework/Framework/ReInvent/codeGENkit/Player.Designer.cs
@@ -0,0 +1,12 @@
+// Generate Id:09f4e5c2-6221-4ab0-83d2-0e70b1ff2b49
+using UnityEngine;
+
+// 1.请在菜单 编辑器扩展/Namespace Settings 里设置命名空间
+// 2.命名空间更改后，生成代码之后，需要把逻辑代码文件（非 Designer）的命名空间手动更改
+namespace QFramework.CodeGenKit
+{
+	public partial class Player
+	{
+
+	}
+}
