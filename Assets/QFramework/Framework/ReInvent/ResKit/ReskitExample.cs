71d3363a3fd0d6a5b555a61ebe4302ab88794497
ilxna <ilvxna@hotmail.com>
2023-02-25 22:05:24 +0800

    add asset boudle

--- Assets/QFramework/Framework/ReInvent/ResKit/ReskitExample.cs
+++ Assets/QFramework/Framework/ReInvent/ResKit/ReskitExample.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace QFramework.res { 
 public class ReskitExample : MonoBehaviour
 {
+
+        private ResLoader resLoader = ResLoader.Allocate();
     // Start is called before the first frame update
     void Start()
     {
-        
+        ResKit.Init();
+
+
     }
 
     // Update is called once per frame
@@ -15,4 +20,5 @@ public class ReskitExample : MonoBehaviour
     {
         
     }
+}
 }
