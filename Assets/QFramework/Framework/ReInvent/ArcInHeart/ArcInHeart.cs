14124f47d8834b14f2947e0c6745a364d9f86b9b
ilxna <ilvxna@hotmail.com>
2023-02-25 11:13:47 +0800

    up

--- Assets/QFramework/Framework/ReInvent/ArcInHeart/ArcInHeart.cs
+++ Assets/QFramework/Framework/ReInvent/ArcInHeart/ArcInHeart.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArcInHeart : MonoBehaviour
+namespace QFramework.ArcinHeart
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ArcInHeart : MonoBehaviour
     {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+	#region ¿ò¼Ü
+    public interface ICommand
     {
-        
+        void Execute();
     }
+
+    public class BindableProperty<T>
+    {
+        public T value;
+    }
+	#endregion
 }
