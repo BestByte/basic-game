ce3f1c1917c78ccf14f84d106fb71d74017d6367
ilxna <ilvxna@hotmail.com>
2023-02-25 20:26:29 +0800

    SequenceAndCallback

--- Assets/QFramework/Framework/ReInvent/ActionKit/SequenceAndCallback.cs
+++ Assets/QFramework/Framework/ReInvent/ActionKit/SequenceAndCallback.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+namespace QFramework.action { 
 public class SequenceAndCallback : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("sequence start£º"+Time.time);
+            ActionKit.Sequence()
+                .Callback(() => Debug.Log("Delay start:" + Time.time)).
+                Delay(1.0f)
+                .Callback(() => Debug.Log("deblay finish:" + Time.time))
+                .Start(this, _ => { Debug.Log("sequence finish£º" + Time.time); });
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+}
 }
