b45035ef82e2a2a726d3dacea52b6e30ff46d80f
ilxna <ilvxna@hotmail.com>
2023-02-25 21:35:21 +0800

    DelayFrameExample

--- Assets/QFramework/Framework/ReInvent/ActionKit/DelayFrameExample.cs
+++ Assets/QFramework/Framework/ReInvent/ActionKit/DelayFrameExample.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+
+using QFramework;
+
 using UnityEngine;
-
-public class DelayFrameExample : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+namespace QFramework.action {
+    public class DelayFrameExample : MonoBehaviour
     {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("Delay Frame Start FrameCount:" + Time.frameCount);
+            ActionKit.DelayFrame(1, () =>
+            {
+                Debug.Log("delya fram finish :" + Time.frameCount);
+            }).Start(this);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            ActionKit.Sequence()
+                .DelayFrame(10)
+                .Callback(() => Debug.Log("sequence delay framecount£º" + Time.frameCount))
+                .Start(this);
+
+            ActionKit.NextFrame(() => { }).Start(this);
+    
     }
+    }
+    
 }
