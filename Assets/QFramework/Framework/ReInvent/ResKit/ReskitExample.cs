635f4db9dfab5773a8a98c4f256fe42cf6406ad8
ilxna <ilvxna@hotmail.com>
2023-02-25 22:28:34 +0800

    ReskitExample

--- Assets/QFramework/Framework/ReInvent/ResKit/ReskitExample.cs
+++ Assets/QFramework/Framework/ReInvent/ResKit/ReskitExample.cs
@@ -17,13 +17,26 @@ public class ReskitExample : MonoBehaviour
             var gameObj=Instantiate(prefab);
             gameObj.name = "这是通过使用assetname 加载的对象";
 
+            // 通过 AssetBundleName 和 资源名搜索并加载资源（更精确）
+            prefab = resLoader.LoadSync<GameObject>("assetobj_prefab", "AssetObj");
+            gameObj=Instantiate(prefab);
+            gameObj.name = " 通过 AssetBundleName 和 资源名搜索并加载资源（更精确）";
 
-    }
 
-    // Update is called once per frame
-    void Update()
+		}
+
+		// Update is called once per frame
+		void Update()
     {
-        
+					
     }
-}
+		private void OnDestroy()
+		{
+			// 释放所有本脚本加载过的资源
+			// 释放只是释放资源的引用
+			// 当资源的引用数量为 0 时，会进行真正的资源卸载操作
+			resLoader.Recycle2Cache();
+			resLoader = null;
+		}
+	}
 }
