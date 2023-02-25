ed424f430e40f47770a682c2242210c509e3b9b5
ilxna <ilvxna@hotmail.com>
2023-02-25 11:33:11 +0800

    框架

--- Assets/QFramework/Framework/ReInvent/ArcInHeart/ArcInHeart.cs
+++ Assets/QFramework/Framework/ReInvent/ArcInHeart/ArcInHeart.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -27,7 +28,23 @@ namespace QFramework.ArcinHeart
 
     public class BindableProperty<T>
     {
-        public T value;
+        private T mValue = default;
+        public T Value
+        {
+            get => mValue;
+            set
+            {
+                if (mValue!=null&&mValue.Equals(value))
+                {
+                    return;
+                }
+                mValue = value;
+
+               OnValueChanged?.Invoke(mValue);
+            }
+        }
+
+        public event Action<T> OnValueChanged = _ => { };
     }
 	#endregion
 }
