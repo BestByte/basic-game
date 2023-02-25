c5ded1f31c0ff9748d345236e318336c488bf3b7
ilxna <ilvxna@hotmail.com>
2023-02-25 16:13:55 +0800

    add gen files ； :art:

---
+++ Assets/QFramework/Framework/ReInvent/codeGENkit/Player.Designer.cs
@@ -0,0 +1,16 @@
+// Generate Id:62bb21f9-88f2-450e-8454-4df0c7be1e64
+using UnityEngine;
+
+// 1.请在菜单 编辑器扩展/Namespace Settings 里设置命名空间
+// 2.命名空间更改后，生成代码之后，需要把逻辑代码文件（非 Designer）的命名空间手动更改
+namespace QFramework.Example
+{
+	public partial class Player
+	{
+
+		public UnityEngine.SpriteRenderer Weapon;
+
+		public Transform GroundCheck;
+
+	}
+}
