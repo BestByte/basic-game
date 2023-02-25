6e92bf79ee1dafe1a52bb8ee14df16b38e2c5f2d
ilxna <ilvxna@hotmail.com>
2023-02-25 22:09:16 +0800

    // 通过资源名 + 类型搜索并加载资源（更方便）：:memo:

--- Assets/QFramework/Framework/ReInvent/ResKit/ReskitExample.cs
+++ Assets/QFramework/Framework/ReInvent/ResKit/ReskitExample.cs
@@ -12,6 +12,10 @@ public class ReskitExample : MonoBehaviour
     {
 			// 项目启动只调用一次即可
 			ResKit.Init();
+			// 通过资源名 + 类型搜索并加载资源（更方便）
+			var prefab = resLoader.LoadSync<GameObject>("AssetObj");
+            var gameObj=Instantiate(prefab);
+            gameObj.name = "这是通过使用assetname 加载的对象";
 
 
     }
