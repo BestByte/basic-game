6f42ef83b9d163b426f59eabfb0ee1c944328bef
ilxna <ilvxna@hotmail.com>
2023-03-04 20:08:01 +0800

    // 2.定义一个架构（提供 MVC、分层、模块管理等:memo:

--- Assets/QFramework/Framework/Examples/7.PointPointPoint/Scripts/Utility/IStorage.cs
+++ Assets/QFramework/Framework/Examples/7.PointPointPoint/Scripts/Utility/IStorage.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-namespace QFramework.CountGame
+namespace QFramework.PointGame
 {
     public interface IStorage : IUtility
     {
--- Assets/QFramework/Framework/ReInvent/PointGame/Scripts/CountGame.cs
+++ Assets/QFramework/Framework/ReInvent/PointGame/Scripts/CountGame.cs
@@ -1,6 +1,7 @@
 ﻿namespace QFramework.CountGame
 {
-    public class PointGame : Architecture<PointGame>
+	// 2.定义一个架构（提供 MVC、分层、模块管理等
+	public class CountGame : Architecture<CountGame>
     {
         protected override void Init()
         {
