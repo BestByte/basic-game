5f676e58240ab5934e472fbdee6db0c6d1245b64
ilxna <ilvxna@hotmail.com>
2023-03-05 13:36:26 +0800

    up

--- Assets/QFramework/Framework/ReInvent/PointGame/Scripts/Command/StartGameCommand.cs
+++ Assets/QFramework/Framework/ReInvent/PointGame/Scripts/Command/StartGameCommand.cs
@@ -8,8 +8,7 @@
             // 重置数据
             var gameModel = this.GetModel<IGameModel>();
           
-            gameModel.KillCount.Value = 0;
-            gameModel.Score.Value = 0;
+        
             
             this.SendEvent<GameStartEvent>();
         }
