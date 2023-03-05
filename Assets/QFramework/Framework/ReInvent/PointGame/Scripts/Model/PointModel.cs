878c4a31cad27f59479ed50fa38ed6af69a2de32
ilxna <ilvxna@hotmail.com>
2023-03-05 16:48:46 +0800

    up

--- Assets/QFramework/Framework/ReInvent/PointGame/Scripts/Model/PointModel.cs
+++ Assets/QFramework/Framework/ReInvent/PointGame/Scripts/Model/PointModel.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class PointModel : MonoBehaviour
+using QFramework;
+namespace CountGame
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public interface IPoin
+    public class PointModel : MonoBehaviour
     {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
