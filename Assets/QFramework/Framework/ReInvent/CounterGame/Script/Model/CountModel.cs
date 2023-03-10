e10468a96beb49d0992cd01404ec7546351fd5b9
ilxna <ilvxna@hotmail.com>
2023-03-10 23:07:22 +0800

    up

--- Assets/QFramework/Framework/ReInvent/CounterGame/Script/Model/CountModel.cs
+++ Assets/QFramework/Framework/ReInvent/CounterGame/Script/Model/CountModel.cs
@@ -9,6 +9,9 @@ namespace CountGame {
     }
 	public class CountModel : AbstractModel, ICountModel
 	{
+		/// <summary>
+		/// count
+		/// </summary>
 		public BindableProperty<int> Count { get; } =new BindableProperty<int>();
 
 		protected override void OnInit()
