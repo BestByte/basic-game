a5facedf92f1953ea90e294cced6feb5ad1bd6d8
ilxna <ilvxna@hotmail.com>
2023-03-11 21:57:20 +0800

    add  UI  gen

---
+++ Assets/QFramework/Framework/ReInvent/CounterGame/Script/ViewController/Canvas.Designer.cs
@@ -0,0 +1,18 @@
+// Generate Id:63355169-ffda-41ed-a40e-9eca381f8a2c
+using UnityEngine;
+
+// 1.请在菜单 编辑器扩展/Namespace Settings 里设置命名空间
+// 2.命名空间更改后，生成代码之后，需要把逻辑代码文件（非 Designer）的命名空间手动更改
+namespace CountGame 
+{
+	public partial class Canvas
+	{
+
+		public UnityEngine.UI.Button ButtonAdd;
+
+		public UnityEngine.UI.Button ButtonSub;
+
+		public UnityEngine.UI.Text CountText ;
+
+	}
+}
