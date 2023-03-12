6b6f80d585e28f3d6f052e880d9fc3723413ffc3
ilxna <ilvxna@hotmail.com>
2023-03-12 20:00:09 +0800

    up

--- Assets/QFramework/Framework/ReInvent/CounterGame/Script/ViewController/CountController.cs
+++ Assets/QFramework/Framework/ReInvent/CounterGame/Script/ViewController/CountController.cs
@@ -1,5 +1,6 @@
 using QFramework;
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,18 +8,54 @@ using UnityEngine.UI;
 
 namespace CountGame
 {
-    public class CountController : MonoBehaviour
+    public class CountController : MonoBehaviour,IController
     {
-       
-		
+		// View
+		private Button mBtnAdd;
+		private Button mBtnSub;
+		private Text mCountText;
+
+
+		private ICountModel mCountModel;
+		public IArchitecture GetArchitecture()
+		{
+            return CountGame.Interface;
+		}
+
+
 		// Start is called before the first frame update
 		void Start()
         {
+            mBtnAdd = this.transform.Find("ButtonAdd").GetComponent<Button>();
+            mBtnSub=this.transform.Find("ButtonSub").GetComponent <Button>();
+            mCountText=this.transform.Find("CountText").GetComponent<Text>();
 
-        }
+			// 监听输入
+			mBtnAdd.onClick.AddListener(() =>
+			{
+				// 交互逻辑
+				this.SendCommand<IncreaseCountCommand>();
+			});
 
-        // Update is called once per frame
-        void Update()
+			mBtnSub.onClick.AddListener(() =>
+			{
+				// 交互逻辑
+				this.SendCommand(new DecreaseCountCommand(/* 这里可以传参（如果有） */));
+			});
+
+			mCountModel.Count.RegisterWithInitValue(newCount =>
+			{
+				UpdateView();
+			}).UnRegisterWhenGameObjectDestroyed(gameObject);
+		}
+
+		private void UpdateView()
+		{
+			mCountText.text=mCountModel.Count.ToString();
+		}
+
+		// Update is called once per frame
+		void Update()
         {
 
         }
