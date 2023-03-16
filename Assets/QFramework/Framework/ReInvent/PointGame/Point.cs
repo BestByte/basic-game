14e9391d05513957a394f605ce069b9fff99a791
ilxna <ilvxna@hotmail.com>
2023-03-16 23:13:15 +0800

    RegisterModel<IGameModel>

--- Assets/QFramework/Framework/ReInvent/PointGame/Point.cs
+++ Assets/QFramework/Framework/ReInvent/PointGame/Point.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using QFramework;
+using QFramework.PointGame;
+
 namespace Point {
 	public class Point : Architecture<Point>
 	{
 		protected override void Init()
 		{
-			
 
 
+			this.RegisterModel<IGameModel>(new PointModel());
 
 			this.RegisterUtility<IStorage>(new PlayerPrefsStorage());
 		}
