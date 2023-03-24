b3afec4758961bc928c0533bda4c69a302cae602
ilxna <ilvxna@hotmail.com>
2023-03-24 20:24:18 +0800

    IAchievementSystem :memo:

---
+++ Assets/QFramework/Framework/ReInvent/PointGame/Scripts/System/IAchievementSystem.cs
@@ -0,0 +1,13 @@
+using QFramework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Point
+{
+
+public interface IAchievementSystem : ISystem
+{
+   
+}
+
+}
