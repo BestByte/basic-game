df502c94788a08e62bef0c1eda8167eb41398a36
ilxna <ilvxna@hotmail.com>
2023-03-24 23:23:34 +0800

    StartGameCommand

--- Assets/QFramework/Framework/ReInvent/PointGame/Scripts/Event/GameStartEvent.cs
+++ Assets/QFramework/Framework/ReInvent/PointGame/Scripts/Event/GameStartEvent.cs
@@ -3,18 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace Point
 {
-    public class GameStartEvent : MonoBehaviour
+    public class GameStartEvent 
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+        
     }
 }
