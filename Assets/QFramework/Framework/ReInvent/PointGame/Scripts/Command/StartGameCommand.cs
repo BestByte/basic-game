73a266a9ffa4ec200a5210087a13c60a831f044e
ilxna <ilvxna@hotmail.com>
2023-03-25 18:31:36 +0800

    StartGameCommand :memo:

--- Assets/QFramework/Framework/ReInvent/PointGame/Scripts/Command/StartGameCommand.cs
+++ Assets/QFramework/Framework/ReInvent/PointGame/Scripts/Command/StartGameCommand.cs
@@ -1,18 +1,13 @@
+using QFramework;
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartGameCommand : MonoBehaviour
+public class StartGameCommand : AbstractCommand
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	protected override void OnExecute()
+	{
+		
+	}
 }
