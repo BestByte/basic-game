24e42f0748b5c494d938c33757fa559cf497f27a
ilxna <ilvxna@hotmail.com>
2023-03-25 21:49:49 +0800

    register  event:memo :

--- Assets/QFramework/Framework/ReInvent/PointGame/Scripts/Game/Game.cs
+++ Assets/QFramework/Framework/ReInvent/PointGame/Scripts/Game/Game.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Game : MonoBehaviour
+using QFramework;
+namespace Point { 
+	/// <summary>
+	/// controller 
+	/// </summary>
+public class Game : MonoBehaviour,IController
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+		public IArchitecture GetArchitecture()
+		{
+			return Point.Interface; 
+		}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		
+		private void OnGameStart(GameStartEvent gameStartEvent)
+		{
+
+		}
+
+		private void Awake()
+		{
+			//register  event 
+			this.RegisterEvent<GameStartEvent>(OnGameStart);
+		}
+	}
 }
